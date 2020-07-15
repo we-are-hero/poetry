f76bcabbc56d1ccc051457b15181a2d6327ab58a
IgnatiusGL <2370291851@qq.com>
2020-07-15 23:03:09 +0800

    配置静态资源路径

--- src/main/java/com/hero/poetry/controller/RankController.java
+++ src/main/java/com/hero/poetry/controller/RankController.java
@@ -4,7 +4,6 @@ import com.hero.poetry.common.utils.R;
 import com.hero.poetry.service.RankService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
